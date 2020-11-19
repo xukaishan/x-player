4826c31d3f4cf9bc876ce3f4e18d23d9b3b92460
xukaishan <369237387@qq.com>
2020-11-19 16:07:05 +0800

    update:1.1.1

--- push.sh
+++ push.sh
@@ -3,9 +3,14 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-
+if [[ -z $1 ]]; then
+  echo "Enter new version: "
+  read -r VERSION
+else
+  VERSION=$1
+fi
 
 git add -A
-git commit -m 'update'
+git commit -m "update:$VERSION"
 
 git push -f git@github.com:xukaishan/x-player.git main:main
